03a0d71bdb27e5fde8c9f910a790e3db4ccde86a
JuanBindez <juanbindez780@gmail.com>
2025-01-19 12:07:00 -0300

    update

--- pytubefix/cli.py
+++ pytubefix/cli.py
@@ -294,38 +294,43 @@ def download_playlist_audio(url):
 
 
 def main():
+    """
+    CLI tool for downloading YouTube videos.
+    """
     parser = argparse.ArgumentParser(description=main.__doc__)
 
-    parser.add_argument("-u", "--url", help="The YouTube /watch or /playlist url", nargs="?")
+    parser.add_argument("-u", "--url", help="The YouTube /watch URL for a video", required=False)
     parser.add_argument("-V", "--version", action="version", version=f"%(prog)s {__version__}")
     parser.add_argument("--itag", type=int, help="The itag for the desired stream")
     parser.add_argument("-r", "--resolution", type=str, help="The resolution for the desired stream")
-    parser.add_argument("-pl", "--playlist", help="download playlist", nargs="?")
-    parser.add_argument("-l", "--list", action="store_true", help="The list option causes pytubefix cli to return a list of streams available to download")
-    parser.add_argument("--oauth", action="store_true", help="use oauth token")
+    parser.add_argument("-pl", "--playlist", help="The URL for a YouTube playlist", required=False)
+    parser.add_argument("-l", "--list", action="store_true", help="List available streams for the provided URL")
+    parser.add_argument("--oauth", action="store_true", help="Use OAuth token")
     parser.add_argument("-v", "--verbose", action="store_true", dest="verbose", help="Set logger output to verbose output.")
-    parser.add_argument("--logfile", action="store", help="logging debug and error messages into a log file")
-    parser.add_argument("--build-playback-report", action="store_true", help="Save the html and js to disk")
-    parser.add_argument("-c", "--caption-code", type=str, help="Download srt captions for given language code. Prints available language codes if no argument given")
-    parser.add_argument('-lc', '--list-captions', action='store_true', help="List available caption codes for a video")
-    #parser.add_argument("-t", "--target", help="The output directory for the downloaded stream. Default is current working directory")
-    parser.add_argument("-a", "--audio", action="store_true", help="Download the audio for a given URL at the highest bitrate available. Defaults to mp4 format if none is specified")
-    parser.add_argument("-f", "--ffmpeg", const="best", nargs="?", help="Downloads the audio and video stream for resolution provided. If no resolution is provided, downloads the best resolution. Runs the command line program ffmpeg to combine the audio and video")
+    parser.add_argument("--logfile", action="store", help="Log debug and error messages into a file")
+    parser.add_argument("--build-playback-report", action="store_true", help="Save the HTML and JS to disk")
+    parser.add_argument("-c", "--caption-code", type=str, help="Download captions for a given language code")
+    parser.add_argument("-lc", "--list-captions", action="store_true", help="List available caption codes for a video")
+    parser.add_argument("-a", "--audio", action="store_true", help="Download the audio for the provided URL")
+    parser.add_argument("-f", "--ffmpeg", const="best", nargs="?", help="Download video and audio using ffmpeg")
 
     args = parser.parse_args()
 
+    # Handle URL-based download
     if args.url:
-
-        if args.playlist:
-            print("ok playlist\n\n\n\n\n")
-            if args.audio:
-                download_playlist_audio(args.url)
-            download_video(args.url)
-            
-        print("url ok\n\n\n\n\n")
         if args.audio:
             download_audio(args.url)
-        download_video(args.url)
+        else:
+            download_video(args.url)
+        print("URL processed successfully!\n")
+
+    # Handle Playlist-based download
+    if args.playlist:
+        if args.audio:
+            download_playlist_audio(args.playlist)
+        else:
+            print(f"Downloading playlist: {args.playlist}")
+        print("Playlist processed successfully!\n")
 
 
 if __name__ == "__main__":
